569c657898319c0babd8fcb7efbc57a260a5f461
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2025-07-04 22:01:14 -0600

    feat: enable fetch

--- src/main/java/serfinsa/com/controller/AuthController.java
+++ src/main/java/serfinsa/com/controller/AuthController.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import serfinsa.com.security.JwtUtils;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class AuthController {
 
     private final UserRepository userRepository;
