668e54139a7609c5adfaca83317f7ce59ced1ffe
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2025-07-04 20:45:17 -0600

    feat: bug with create product

--- src/main/java/serfinsa/com/entity/Product.java
+++ src/main/java/serfinsa/com/entity/Product.java
@@ -2,6 +2,7 @@ package serfinsa.com.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -13,7 +14,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class Product {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String description;
