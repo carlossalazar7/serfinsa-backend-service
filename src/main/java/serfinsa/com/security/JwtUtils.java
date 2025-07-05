bd6bbee965a5a72d20e7bc4253cd7a4513b3d70b
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2025-07-04 20:41:29 -0600

    feat: bug with generate token

--- src/main/java/serfinsa/com/security/JwtUtils.java
+++ src/main/java/serfinsa/com/security/JwtUtils.java
@@ -3,33 +3,46 @@ package serfinsa.com.security;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
 public class JwtUtils {
 
-    private final String jwtSecret = "mi_clave_super_secreta";
+    private SecretKey jwtSecret;
     private final long jwtExpirationMs = 86400000;
+
+    @PostConstruct
+    public void init() {
+        jwtSecret = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+    }
 
     public String generateJwtToken(UserDetails userDetails) {
         return Jwts.builder()
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-                .signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpirationMs))
+                .signWith(jwtSecret)
                 .compact();
     }
 
     public String getUserNameFromJwtToken(String token) {
-        return Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody().getSubject();
+        return Jwts.parserBuilder()
+                .setSigningKey(jwtSecret)
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
     }
 
     public boolean validateJwtToken(String authToken) {
         try {
-            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(authToken);
             return true;
         } catch (JwtException e) {
             return false;
