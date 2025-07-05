72368804a69da00da82270cd898cb6c7ad335dc9
Carlos Eduardo Peñate Salazar <carlos_penate@siman.com>
2025-07-04 23:15:59 -0600

    feat: bug with angular

--- src/main/java/serfinsa/com/configuration/SecurityConfig.java
+++ src/main/java/serfinsa/com/configuration/SecurityConfig.java
@@ -57,10 +57,9 @@ public class SecurityConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
             }
         };
     }
